cbfc5d7c645f0e3204ff530e13ef03c4cee6b758
Manu1175 <manusp33@hotmail.com>
2025-07-05 17:48:53 +0200

    updated rounding

--- predict/prediction.py
+++ predict/prediction.py
@@ -5,12 +5,13 @@ import numpy as np
 # Load the model once at module level
 model = joblib.load("model/catboost_model.joblib")
 
-def predict_price(data: np.ndarray) -> float:
+def predict_price(data: np.ndarray) -> int:
     """Make a price prediction from preprocessed input"""
     prediction = model.predict(data)
+    log_price = prediction[0]
     # transforms log price
-    final_price = np.expm1(prediction)
-    return float(final_price)
+    final_price = np.expm1(log_price)
+    return int(round(final_price))
 
 #prediction[0]
 
