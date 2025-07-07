bd0a316ed386a252f989beeebbae40481aecec9c
Manu1175 <manusp33@hotmail.com>
2025-07-07 09:58:01 +0200

    updated local api

--- predict/prediction.py
+++ predict/prediction.py
@@ -3,18 +3,21 @@ import joblib
 import numpy as np
 
 # Load the model once at module level
-model = joblib.load("model/catboost_model.joblib")
+model = joblib.load("model/catboost.joblib")
 
-def predict_price(data: np.ndarray) -> int:
+def predict_price(data: np.ndarray) -> float:
     """Make a price prediction from preprocessed input"""
     prediction = model.predict(data)
     log_price = prediction[0]
     # transforms log price
     final_price = np.expm1(log_price)
-    return int(round(final_price))
+    return float(round(final_price, 2))
+
+
 
 #prediction[0]
 
 #return float(final_price)
+# return int(round(final_price))
 
     
--- preprocessing/cleaning_data.py
+++ preprocessing/cleaning_data.py
@@ -1,9 +1,6 @@
-from pydantic import BaseModel, model_validator
 import pandas as pd
 import numpy as np
 import re
-import json
-from sklearn.preprocessing import OrdinalEncoder
 
 def is_valid_belgian_postcode(postcode: str) -> bool:
     if not re.fullmatch(r"\d{4}", postcode):
@@ -35,15 +32,6 @@ def latitude_longitude_columns(df, filepath2):
         df.drop(columns=['zip_code'], inplace=True)
         return df
 
-def determine_region(postcode: int) -> str:
-    """Returns region per postal code"""
-    if 1000 <= postcode <= 1299:
-        return "Brussels"
-    elif postcode >= 1300 and postcode <= 1499 or postcode >= 4000 and postcode <= 7999:
-        return "Wallonia"
-    else:
-        return "Flanders"
-
 def add_region_columns(df):
     # Déduire région en fonction du zip_code (exemple simplifié)
     zip_code = df['zip_code'].iloc[0]
@@ -65,20 +53,13 @@ def preprocess(input_dict: dict):
 
     # Create DataFrame
     df = pd.DataFrame([input_dict])
-    
-    """# One-hot encoding (3 columns)
-    region_dummies = pd.get_dummies(df["region"], prefix="region")
-    df = pd.concat([df, region_dummies], axis=1)
-    df.drop(columns=["region"], inplace=True)"""
 
     # Fill missing optional fields with defaults
     optional_fields = {
         "garden": False,
         "swimming_pool": False,
         "terrace": False,
-        'parking': False,
-        'epc_score': 'E',
-        "building_state": 'GOOD'
+        "building_state": 'TO BE DONE UP'
     }
     for key, default in optional_fields.items():
         if key not in df:
@@ -86,25 +67,11 @@ def preprocess(input_dict: dict):
         df[key] = df[key].fillna(default)
 
     # Boolean fields to int
-    bool_fields = ["garden", "swimming_pool", "terrace", 'parking']
+    bool_fields = ["garden", "swimming_pool", "terrace"] # parking
     for field in bool_fields:
         df[field] = df[field].astype(int)
     
     # Pre processing EPC Score and Building condition
-    df['epc_score'] = df['epc_score'].map({
-        'A++': 0.0, 
-        'A+': 1.0, 
-        'A': 2.0, 
-        'B': 3.0, 
-        'C': 4.0, 
-        'D': 5.0, 
-        'E': 6.0, 
-        'F': 7.0, 
-        'G': 8.0
-    })
-    if df["epc_score"].isna().any():
-        raise ValueError("Invalid epc_score")
-    
     df["building_state"] = df["building_state"].map({
         "NEW": 0,
         "JUST RENOVATED": 1,
@@ -114,36 +81,20 @@ def preprocess(input_dict: dict):
         "TO RESTORE": 5
     })
     if df["building_state"].isna().any():
-        raise ValueError("Invalid building-state")
+        raise ValueError("Invalid building_state")
     
     # Map categorical fields to numeric
     df["property_type"] = df["property_type"].map({"APARTMENT": 0, "HOUSE": 1})
     if df["property_type"].isna().any():
-        raise ValueError("Invalid property-type")
+        raise ValueError("Invalid property_type")
     
     # Add columns per "region"
     df = add_region_columns(df)
     
     # Checking valid postcode and transforming in latitude and longitude
     if not is_valid_belgian_postcode(str(df['zip_code'].iloc[0])):
-        raise ValueError("Invalid postcode")
+        raise ValueError("Invalid zip_code")
     else:
         df=latitude_longitude_columns(df, 'data/georef-belgium-postal-codes@public.csv')    
     
-    # Save to file (optional)
-    #df.to_json("data_preprocessed.json", orient="records", indent=2)
-
-    # Return as Python object
     return df.values # returns a ndarray
-
-"""print(preprocess({
-  "rooms_number": 4,
-  "zip_code": 1000,
-  "property_type": "HOUSE",
-  "building_state": "GOOD",
-  "garden": True,
-  "terrace": True,
-  "swimming_pool": False,
-  "equipped_kitchen": True
-}))"""
-    #df.to_dict(orient="records")
